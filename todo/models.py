0385ab7c0656afe3f0c203e2cb2d7e059ec969ea
Adelina Lee <adelinaleett@gmail.com>
2021-12-01 21:34:16 +0800

    Added blank=true to datecompleted in Todo's model.

--- todo/models.py
+++ todo/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField(blank=True)  # blank=True - not compulsory
-    created = models.DateTimeField(auto_now_add=True, blank=True)  # Instantly specified created value at time
-    datecompleted = models.DateTimeField(null=True)  # After clicking complete
+    created = models.DateTimeField(auto_now_add=True)  # Instantly specified created value at time
+    datecompleted = models.DateTimeField(null=True, blank=True)  # After clicking complete
     important = models.BooleanField(default=False)  # Mark important
     # Stores relationship with this to do and user - one to many relationship
     # One user can have many todos
