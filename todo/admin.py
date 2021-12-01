220061ac65ff67bad55ee6d2c4a88d001467d430
Adelina Lee <adelinaleett@gmail.com>
2021-12-01 21:39:29 +0800

    Added TodoAdmin class in admin.py.

--- todo/admin.py
+++ todo/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Todo
 
-admin.site.register(Todo)
+
+# Customize what the admin UI looks like
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+
+admin.site.register(Todo, TodoAdmin)
